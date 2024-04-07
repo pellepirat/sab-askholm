588088d0bb3ad1c8b18a2ccd51ee12739bb492fa
pellepirat <niklas@munk-andersen.dk>
2024-04-07 10:58:14 +0200

    NewHomepage v0.1 - layout styling and responsiveness

---
+++ referat.js
@@ -0,0 +1,51 @@
+import referater from './referater.js';
+
+// Infobox toggle
+const infoBoxButton = document.getElementById('info-box-toggle');
+const infoBox = document.getElementById('info-box');
+const referatButton = document.getElementById('referatButton');
+
+function toggleInfoBox() {
+    infoBox.classList.toggle('hidden'); 
+}
+
+infoBoxButton.addEventListener('click', toggleInfoBox);
+
+
+
+// Update the info box with 'referat'
+
+    // function to alter infobox
+function updateInfoBox(referatId) {
+    const infoBoxTitle = document.getElementById('info-box-title')
+    const infoBoxTime = document.getElementById('time')
+    const infoBoxAttendees = document.getElementById('participant')
+    const infoBoxText = document.getElementById('info-box-text')
+        
+    const referat = referater.find(r => r.id === referatId);
+
+    if (referat) {
+    infoBoxTitle.textContent = 'Bestyrelsesreferat'
+    infoBoxTime.innerHTML = 'Tidspunkt: ' + referat.tid;
+    infoBoxAttendees.textContent = 'Deltagere: ' + referat.attendees;
+    infoBoxText.innerHTML = referat.text;
+    } else {
+        console.error('Referat not found', referatId);
+    }
+}
+ 
+// Get all buttons with the referatButton class
+const referatButtons = document.getElementsByClassName('referatButton');
+
+// Add click event listeners to each button
+for (let button of referatButtons) {
+    button.addEventListener('click', function() {
+        // Extract the referat ID from the button's ID
+        const referatId = this.id;
+        updateInfoBox(referatId);
+    });
+}
+
+for (let button of referatButtons) {
+    button.addEventListener('click', toggleInfoBox);
+}
---
+++ referater.js
@@ -0,0 +1,150 @@
+let referater = [
+    {       
+        id: 'brev-apr-24',
+        tid: 'April 2024',
+        attendees: '-',
+        text: ` Tekst kommer snart
+        `
+    },
+    {       
+    id: 'best-jan-24',
+    tid: '3. Januar 2024',
+    attendees: 'NMA, JR, BB, MER, ITG',
+    text: `
+    <h4>Opfølgning på sidste møde:</h4>
+        <p>Bestyrelsen har fulgt op på aktiviteter siden sidste møde. 
+          Der har været bestyrelseskursus hvor Niklas har deltaget, samt område møde for SAB gruppen Vanløse. Her deltog formanden, Niklas. <br>
+          Generelt blev der drøftet forskellige aspekter af SAB, og luft bekymringer for serviceniveauet efter at driften er slået sammen mellem Vanløse afdelingerne og Sluseholmen. 
+          <br>
+          Driften melder, at de ser på pærer i opgange, samt at der henstilles til at man ikke opbevarer ting i opgangen. Efterladte genstande i opgangen vil blive fjernet af driftspersonalet. 
+          </p>
+    
+        <h4>Vandindtrængen Parkeringskælder: </h4>
+        <p>Jens kontakter KAB/SAB ang. hjælp til at søge støtte hos byggeskadefonden/landsbyggefonden
+          <br>Gårdlauget indrages for at klarlægge plan for udbedringer. 
+          </p>
+    
+        
+        <h4>Cykelparkering Dexter Gordons Vej:</h4>
+        <p>Der opleves udfordringer med parkerede ladcykler på Dexter Gordons Vej. Bestyrelsen vil gerne understrege, at parkering langs murene er forbudt, og der henvises til særskilte cykelparkering med stativer, samt parkering af ladcykler i kælderen.
+          <br>Der opstartes en dialog med gårdlauget angående cykeloprydning i kælderen for at skabe plads til flere cykler. 
+          
+        <h4>Kommunikation om bestyrelsen:</h4>
+          <p>Bestyrelsen vil arbejde på at udsende information til beboerne ang. bestyrelsesarbejde, samt ophængning af bestyrelsesreferat.</p>
+
+  
+        <h4>Næste møde:</h4>
+        <p>Næste møde afholdes 7. februar kl. 19, og bestyrelsen modtager som altid gerne forslag el. andet der skal drøftes. 
+        </p>
+    
+        <h4>Evt.:</h4>
+    
+        <p>Grundejerforeningen afholder generalforsamling i maj, og der påtænkes at planlægge forslag stilling således vi får indflydelse på området. 
+
+          Samtidig bemærkes det også at beboere opfordres til at stille forslag til bestyrelsen såfremt man ønsker at tage noget på afdelingsmødet i marts. </p>
+    `
+}, 
+{       
+    id: 'best-apr-24',
+    tid: '3. April 2024',
+    attendees: 'NMA, BB, ITG',    
+    text: 'Referat kommer snart'
+}, 
+{       
+    id: 'best-nov-23',
+    tid: '8. November 2023',
+    attendees: 'NMA, MR, ITG, BB',    
+    text: `<h4>Opfølgning på sidste møde:</h4>
+    <p>Bestyrelsen har drøftet sager og fulgt op på punkter fra det seneste møde.</p>
+
+    <h4>Markvandring + Økonomi:</h4>
+    <p>Formanden informerede om markvandringen, hvor der blev drøftet driftsbudgetter og planlagt vedligeholdelse. Nogle fejl i den nuværende planlagte vedligeholdelse blev identificeret og vil blive rettet inden det endelige budgetforslag for 2024/2025.</p>
+
+    <h5>Tjek op på medfinansiering af udgifter til p-kælder:</h5>
+    <p>Jens vil evt. kontakte Byggeskadefonden igen for at klarlægge mulighederne for medfinansiering af udgifter til p-kælder.</p>
+
+    <h5>Generelt om Økonomi:</h5>
+    <p>Vi har vendt generelle økonomipunkter, og alle bestyrelsesmedlemmer opfordres til at dykke ned i økonomien på mitKAB bestyrelsesportal. Vi kan evt. undersøge ejendomsværdi ift. ny beskatning - det betyder nok ikke det store så længe ejendommen ikke sælges.</p>
+
+    <h4>Hjemmeside + information fra bestyrelsen:</h4>
+    <p>Niklas arbejder på at udsende information fra bestyrelsen og fremhæve hjemmesiden som en central platform for kommende kommunikation. Der refereres til et tidligere skriv udarbejdet af Kristina vedrørende et velkomstbrev.</p>
+
+    <h4>Næste møde:</h4>
+    <p>Indkaldelse sendes ud til næste møde den 6. december kl. 19.00.</p>
+
+    <h4>Evt.:</h4>
+
+    <h5>Niveau på service center:</h5>
+    <p>Opfordring til beboere om at rapportere god eller dårlig service fra ejendomskontoret.</p>
+
+    <h5>Festlokale:</h5>
+    <p>Problemer med støjende fester og overskridelse af husordenen diskuteres. Birgitte arbejder på at udarbejde en "husorden" for lokalerne, som skal sendes til lejere. Forsøg på at skaffe et driftsbudget isoleret for beboerlokale+gæstebolig.</p>
+`
+}, 
+{       
+    id: 'best-okt-23',
+    tid: '10. Oktober 2023',
+    attendees: 'NMA, MR, ITG, BB',    
+    text: `
+    <h4>Opfølgning på sidste møde:</h4>
+        <p>Bestyrelsen har drøftet sager og fulgt op på punkter fra det seneste møde.</p>
+    
+        <h4>Information om SAB og organisationen generelt ved Lise Buhelt:</h4>
+        <p>Besøg af Lise Buhelt fra SAB, hvor hun præsenterede organisationen og muligheder for samarbejde. Der er potentiale for bedre økonomisk overblik og øget synlighed for bestyrelsen.</p>
+    
+        <h4>Opsøgende arbejde og bestyrelsens synlighed:</h4>
+        <p>Diskussion om nødvendigheden af opsøgende arbejde for at informere nye beboere om bestyrelsens rolle og synlighed. Forslag om at arrangere dialogskabende events som gløgg- eller pølseaftener for at involvere beboerne og øge bestyrelsens synlighed.</p>
+    
+        <h4>Cykeloprydning og -parkering:</h4>
+        <p>Overvejelser om etablering af cykelparkering, især foran børnehuset på Sluseholmen. Bestyrelsens anbefaling er at undgå parkering foran børnehuset på grund af trængsel i morgen- og eftermiddagstimer. I stedet anbefales parkering på hjørnerne ved DGV og TJV.</p>
+    
+        <h4>Beboerlokale og gæsteværelse:</h4>
+        <p>Diskussion om indførelse af depositum som garanti fra gårdlauget i forbindelse med brug af beboerlokaler og gæsteværelse. Behov for yderligere økonomisk analyse af beboerlokale og gæsteværelse.</p>
+    
+        <h4>Booking system og økonomi:</h4>
+        <p>Overvejelser om at implementere et booking system gennem SAB for beboerlokale og gæsteværelse. Nødvendighed af økonomisk gennemgang og opdatering af disse faciliteter.</p>
+    
+        <h4>Genbrugsstation og affaldssortering:</h4>
+        <p>Beslutning om at tage kontakt for at undersøge muligheder for forbedringer i forbindelse med genbrugsstationen og affaldssortering.</p>
+    
+        <h4>Driftkonti og økonomi:</h4>
+        <p>Drøftelse af driftskonti hos KAB. Spørgsmål vedrørende driftsøkonomi rettes til Jesper. Anmodning om økonomisk gennemgang i kommende møder.</p>
+    
+        <h4>Markvandring og istandsættelse:</h4>
+        <p>Planlægning af en markvandring for at identificere områder, der kræver istandsættelse, samt udarbejdelse af budget. Gennemgang af forventningerne til, hvad servicecenteret skal levere til beboerne. Forslag om at forberede og diskutere serviceniveau.</p>
+    
+        <h4>Bestyrelseskursus og seminar:</h4>
+        <p>Overvejelser om deltagelse i kurser og arrangement af et bestyrelsesseminar i Vanløse-området. Mulighed for deltagelse i forskellige kurser gennem KAB, herunder "Ny i bestyrelsen"-kursus og økonomikursus.</p>
+    
+        <h4>Generelt:</h4>
+        <p>Beslutning om at vælge repræsentantskab af bestyrelsen frem for personlig repræsentation, da det giver større fleksibilitet og mulighed for afstemninger i løbet af året. Fastlæggelse af næste mødedato den 8. november eller muligvis den første onsdag i hver måned.</p>`
+}, 
+{       
+    id: 'best-jun-23',
+    tid: '23. Juni 2023',
+    attendees: 'NMA, JR, MR, ITG, BB',    
+    text: `<h4>Beboerlokale og Gæstebolig</h4>
+    <p>Der blev drøftet udfordringer vedrørende drift af beboerlokalet og gæsteboligen.</p>
+    
+    <h4>Cykelparkering på hjørnerne af karréen</h4>
+    <p>Spørgsmål om ejerskab af cykelparkeringen og muligheder for oprydning blev diskuteret. Kontakten til gårdlaug og grundejerforening blev nævnt som mulige løsninger.</p>
+    
+    <h4>Gårdlaug</h4>
+    <p>Bestyrelsen igangsætter arbejde for at afklare økonomien ved udbedring af membranen i gården. Der var også et ønske om bedre kommunikation mellem bestyrelsen og gårdlauget.</p>
+    
+    <h4>Hjemmeside</h4>
+    <p>Bestyrelsen ønsker at få klarhed over ejerskabet af hjemmesiden. Alternativt overvejes oprettelse af en ny hjemmeside. Kontakten til KAB blev nævnt for at få dokumentation eller mulighed for oprettelse af hjemmesiden.</p>
+    
+    <h4>Synlighed og beboerkommunikation</h4>
+    <p>Muligheden for at oprette et nyhedsbrev blev drøftet i forbindelse med hjemmesiden. Der var også ønske om bedre informationsudveksling til beboerne, herunder oplysningskampagner ved indflytning.</p>
+    
+    <h4>Opgange</h4>
+    <p>Der blev diskuteret lugtgener og rygning, bl.a. i opgangene. Muligheden for bedre ventilation for at forbedre indeklimaet blev nævnt.</p>
+    
+    <h4>Bestyrelsen generelt</h4>
+    <p>Niklas Munk-Andersen blev konstitueret som formand, og der var en fælles beslutning om at arbejde på at etablere et mere stabilt og struktureret system for bestyrelsesarbejdet. Dette inkluderer oprettelse af en platform med kontakt, fildeling og digital infrastruktur. Der blev også talt om at undersøge muligheder som Google Workspace eller lignende, samt kontakte KAB vedrørende finansiering af disse initiativer.</p>
+    
+    `
+}];
+
+export default referater;
